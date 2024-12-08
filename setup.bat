31df6bfd7f57a359a79682a0f3bc7279ff4923e5
Kurama <60360593+Kurama250@users.noreply.github.com>
2024-12-08 01:55:41 +0100

    Create setup.bat

---
+++ setup.bat
@@ -0,0 +1,25 @@
+@echo off
+
+node --version >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Node.js is not installed on your system. Please install Node.js.
+    exit /b 1
+)
+
+npm list fs-extra net user-agents electron electron-builder >nul 2>&1
+if %errorlevel% neq 0 (
+    echo fs-extra net user-agents electron electron-builder module is not installed. Installing them...
+    npm install fs-extra@11.2.0 net@1.0.2 user-agents@1.1.374 electron@33.2.1 electron-builder@25.1.8
+    if %errorlevel% equ 0 (
+        echo Installation of fs-extra net user-agents electron electron-builder completed successfully !
+    ) else (
+        echo An error occurred while installing the fs-extra net user-agents electron electron-builder modules.
+    )
+) else (
+    echo fs-extra net user-agents electron electron-builder modules are already installed.
+)
+
+echo All necessary components are installed.
+echo You can now proceed with your tasks.
+
+pause
